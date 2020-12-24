8a925b272c5455eecd5566632c5d375089741d4b
bhavinbhadani <bhavinbhadani@yahoo.in>
2020-12-24 11:43:50 +0530

    initial commit

---
+++ LoginSwiftUI/LoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIApp.swift
+//  LoginSwiftUI
+//
+//  Created by Appernaut on 24/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
